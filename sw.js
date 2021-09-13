a26deb0357e89947b3e4d257412717788198bb9b
zapalniczki <przemek.rychlewicz@gmail.com>
2021-09-13 20:00:45 +0000

    search

--- sw.js
+++ sw.js
@@ -5097,7 +5097,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 
 
-precacheAndRoute([{'revision':'7e1119cb4da1987dedb858e8ca568545','url':'/404.html'},{'revision':null,'url':'/images/banner-guy.fbf4f0f7396fe31ca288dc1dd9822342.png'},{'revision':'a21c9de3bf40e6059b2bbfaf734a80fe','url':'/index.html'},{'revision':null,'url':'/js/main.18df2e677a8698bf5fa4.js'},{'revision':null,'url':'/js/vendor.c8d3799ca8a2c30d68f3.js'},{'revision':'b127f7589a1b597b853947114cce97f1','url':'/static/banner.jpg'},{'revision':'f866913f47c4b6b837a6920189f8b265','url':'/static/creditcards.png'},{'revision':'c1558c012afbfb5c95a86f1a99786826','url':'/static/favicon.png'},{'revision':'6cb5664eba350b2d6c6bd0c25ad53d08','url':'/static/logo-wordmark.png'},{'revision':'e98996f73a68bf261ecba1f4fe8057af','url':'/vendor.css'}]);
+precacheAndRoute([{'revision':'5b8e16b2c46122cc8966399588d20457','url':'/404.html'},{'revision':null,'url':'/images/banner-guy.fbf4f0f7396fe31ca288dc1dd9822342.png'},{'revision':'b47de2bd4530e1ff34195bb7e3fad2a0','url':'/index.html'},{'revision':null,'url':'/js/main.98f583b65e4a0d39b4a5.js'},{'revision':null,'url':'/js/vendor.c8d3799ca8a2c30d68f3.js'},{'revision':'b127f7589a1b597b853947114cce97f1','url':'/static/banner.jpg'},{'revision':'f866913f47c4b6b837a6920189f8b265','url':'/static/creditcards.png'},{'revision':'c1558c012afbfb5c95a86f1a99786826','url':'/static/favicon.png'},{'revision':'6cb5664eba350b2d6c6bd0c25ad53d08','url':'/static/logo-wordmark.png'},{'revision':'e98996f73a68bf261ecba1f4fe8057af','url':'/vendor.css'}]);
 
 var currentCacheNames = _objectSpread({
   precacheTemp: "".concat(workbox_core_cacheNames_cacheNames.precache, "-temp")
