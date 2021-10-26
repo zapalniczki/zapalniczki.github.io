2a69c32a1fa072d86c9ec3d498f067af8ac060bc
Przemysław Rychlewicz <prychlewicz@speednet.pl>
2021-10-26 17:39:53 +0200

    Badge

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -6,23 +6,27 @@ import getSpace from 'styles/getSpace'
 
 type BaseProps = {
   children?: ReactNode
-  stanalone?: boolean
 }
 
 type Props = BaseProps &
-  ({ count: number; hasText?: false } | { hasText: true; text: string })
+  (
+    | {
+        count: number
+        hasText?: false
+      }
+    | {
+        hasText: true
+        text: string
+      }
+  )
 
-const Badge = ({ children, stanalone = false, ...props }: Props) => {
+const Badge = ({ children, ...props }: Props) => {
   let content: JSX.Element | null = null
   if (props.hasText) {
-    content = (
-      <Count hasText={props.hasText} stanalone={stanalone}>
-        {props.text}
-      </Count>
-    )
+    content = <Count hasText={props.hasText}>{props.text}</Count>
   } else {
     if (props.count >= 1) {
-      content = <Count stanalone={stanalone}>{props.count}</Count>
+      content = <Count count={props.count}>{props.count}</Count>
     }
   }
 
@@ -36,26 +40,26 @@ const Badge = ({ children, stanalone = false, ...props }: Props) => {
 }
 
 type CountProps = {
+  count?: number
   hasText?: boolean
-  stanalone: boolean
 }
 
 const Count = styled.span<CountProps>`
   height: 2rem;
-  width: ${(props) => (props.hasText ? 'unset' : '2rem')};
-  padding: ${(props) => props.hasText && `0 ${getSpace('xxs-size')(props)}`};
-  border-radius: ${(props) => (props.hasText ? '0.3rem' : '55%')};
-  background: ${getColor('red')};
+  min-width: 2rem;
+  padding: ${(props) => `0 ${getSpace('xxs-size')(props)}`};
+  border-radius: 0.3rem;
+  background: ${getColor('braty-red')};
   display: flex;
   justify-content: center;
   align-items: center;
   color: ${getColor('white')};
   font-size: 1.1rem;
   font-weight: bold;
-  position: ${(props) => !props.stanalone && 'absolute'};
-  top: ${(props) => !props.stanalone && '0'};
-  right: ${(props) => !props.stanalone && '0'};
-  transform: ${(props) => !props.stanalone && 'translate(75%, -75%)'};
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(50%, -85%);
 `
 
 export default Badge
