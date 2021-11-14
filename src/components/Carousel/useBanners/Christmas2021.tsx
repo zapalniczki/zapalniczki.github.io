d282336dc34df4d461f140792a481a7aff17e471
Przemysław Rychlewicz <prychlewicz@speednet.pl>
2021-11-14 19:30:58 +0100

    Animation

--- src/components/Carousel/useBanners/Christmas2021.tsx
+++ src/components/Carousel/useBanners/Christmas2021.tsx
@@ -17,35 +17,48 @@ const blink = keyframes`
 
 `
 
+const blinkAlternate = keyframes`
+  20%, 40%, 60%, 80%, 100% {
+    opacity: 1;
+    filter: blur(5px);
+  }
+
+  10%, 30%, 50%, 70%, 90% {
+    opacity: 0;
+    filter: blur(0px);
+  }
+
+`
+
 const Container = styled(Svg)`
   #star-1 {
-    animation: ${blink} 5s ease-in-out infinite alternate;
-    animation-delay: 1s;
+    animation: ${blink} 4s ease-in-out infinite alternate;
+    animation-delay: 0.26s;
   }
 
   #star-2 {
-    animation: ${blink} 5s ease-in-out infinite alternate;
-    animation-delay: 0.5s;
+    animation: ${blinkAlternate} 5s ease-in-out infinite alternate;
+    animation-delay: 0.14s;
   }
 
   #star-3 {
-    animation: ${blink} 5s ease-in-out infinite alternate;
+    animation: ${blink} 3s ease-in-out infinite alternate;
     animation-delay: 0s;
   }
 
   #star-4 {
-    animation: ${blink} 5s ease-in-out infinite alternate;
-    animation-delay: 1.5s;
+    animation: ${blinkAlternate} 6s ease-in-out infinite alternate;
+    animation-delay: 0.41s;
   }
 
   #star-5 {
-    animation: ${blink} 5s ease-in-out infinite alternate;
-    animation-delay: 2s;
+    animation: ${blink} 4.5s ease-in-out infinite alternate;
+    animation-delay: 1.03s;
   }
 
   #star-6 {
-    animation: ${blink} 5s ease-in-out infinite alternate;
-    animation-delay: 2.5s;
+    animation: ${blinkAlternate} 7.2s ease-in-out infinite alternate;
+    animation-delay: 1.11s;
   }
 `
 
