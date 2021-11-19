e3d32fd7c6e4aade3725d2fae572496aa1d8c570
Przemysław Rychlewicz <prychlewicz@speednet.pl>
2021-11-19 11:43:48 +0100

    Logo layout

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,15 +1,18 @@
-import { Box } from 'components'
 import React from 'react'
 import { ReactComponent as Svg } from 'assets/logo.svg'
+import styled from 'styled-components'
 
 type Props = {
   expanded?: boolean
 }
 
-const Logo = ({ expanded }: Props) => (
-  <Box height={!expanded ? '6rem' : '4.5rem'} marginLeft="-1rem">
-    <Svg />
-  </Box>
-)
+const Logo = ({ expanded }: Props) => <StyledSvg $expanded={expanded} />
+
+type StyledSvgProps = { $expanded?: Props['expanded'] }
+const StyledSvg = styled(Svg)<StyledSvgProps>`
+  width: auto;
+  height: ${(props) => (props.$expanded ? '4.5rem' : '6rem')};
+  margin-left: -1rem;
+`
 
 export default Logo
