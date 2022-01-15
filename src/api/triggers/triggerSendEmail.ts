d7eb0de22e836d1e1a620220d5c847106496410f
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-15 12:13:27 +0100

    Upgraded braty-common

--- src/api/triggers/triggerSendEmail.ts
+++ src/api/triggers/triggerSendEmail.ts
@@ -6,7 +6,6 @@ export const triggerSendEmail = (payload: Payload) => {
   const headers = new Headers()
   headers.append('Content-Type', 'application/json')
 
-  // const path = 'https://en1oerk9p7kdazn.m.pipedream.net'
   const path = API.SEND_EMAIL
   const options = {
     method: 'POST',
