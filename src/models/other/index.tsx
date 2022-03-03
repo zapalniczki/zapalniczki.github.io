3463c93faef9c208124fa7e042d2cf4fc8769103
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-03-03 20:03:16 +0000

    Cleanup

--- src/models/other/index.tsx
+++ src/models/other/index.tsx
@@ -1,9 +1,6 @@
 export { default as basketItem } from './basketItem'
 export * from './basketItem'
 
-export { default as timestampDate } from './timestampDate'
-export * from './timestampDate'
-
 export { default as shipping } from './shipping'
 export * from './shipping'
 
